03001625939eb6335493c0242b4d25202b89c34c
perlancar (on pc-home) <perlancar@gmail.com>
2019-08-17 19:54:03 +0700

    Initial commit

---
+++ lib/App/FileTestUtils.pm
@@ -0,0 +1,23 @@
+package App::FileTestUtils;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: More CLIs for file testing
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities which are
+related to file testing:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+The file testing operators in L<perlfunc>, e.g. C<-s>, C<-x>, C<-r>, etc.
+
+L<File::MoreUtil>
+
+=cut
